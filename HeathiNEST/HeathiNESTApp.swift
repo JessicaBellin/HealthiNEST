cb0eba56ef61fc85f9d326ef157cef5414d4eb3d
Jessica Bellin <z5473291@ad.unsw.edu.au>
2024-06-29 10:21:09 +1000

    Initial Commit

---
+++ HeathiNEST/HeathiNESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeathiNESTApp.swift
+//  HeathiNEST
+//
+//  Created by Jessica Bellin on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HeathiNESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
