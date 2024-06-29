862e87f87374539dce087960d13e44edf9339b19
Jessica Bellin <z5473291@ad.unsw.edu.au>
2024-06-30 01:50:24 +1000

    Initial Commit

---
+++ HealthiNest/HealthiNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthiNestApp.swift
+//  HealthiNest
+//
+//  Created by Jessica Bellin on 30/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthiNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
