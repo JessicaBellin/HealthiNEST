3e1fa2058e6e5587aed2fad349bc75557cbb61e2
Jessica Bellin <z5473291@ad.unsw.edu.au>
2024-06-29 13:03:00 +1000

    user's registration and login

--- HeathiNEST/Core/Authentication/LoginView.swift
+++ HeathiNEST/Core/Authentication/LoginView.swift
@@ -4,12 +4,69 @@
 //
 //  Created by Jessica Bellin on 29/06/24.
 //
-
+// Wrap inside navigation stack = move back and forth between login and sign up view
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                // image
+                Image("temp-logo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 120)
+                    .padding(.vertical, 32)
+                
+                // form fields
+                VStack(spacing: 24) {
+                    InputView(text: $email,
+                              title: "Email Address",
+                              placeholder: "name@example.com")
+                    .textInputAutocapitalization(.none)
+                    
+                    InputView(text: $password,
+                              title: "Password",
+                              placeholder: "Enter your password",
+                              isSecureField: true)
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
+                
+                // sign in button
+                Button {
+                    print("Log user in..")
+                } label: {
+                    HStack {
+                        Text("SIGN IN")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundStyle(.white)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                }
+                .background(Color(.systemPurple))
+                .cornerRadius(10)
+                .padding(.top, 24)
+                
+                Spacer()
+                // sign up button
+                NavigationLink{
+                    RegistrationView()
+                        // Delete back button
+                        .navigationBarBackButtonHidden(true)
+                } label: {
+                    HStack(spacing: 10) {
+                        Text("Don't have an account?")
+                        Text("Sign up")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size: 14))
+                }
+            }
+        }
     }
 }
 
--- HeathiNEST/Core/Authentication/RegistrationView.swift
+++ HeathiNEST/Core/Authentication/RegistrationView.swift
@@ -8,8 +8,73 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @State private var email = ""
+    @State private var fullname = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // image
+            Image("temp-logo")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 100, height: 120)
+                .padding(.vertical, 32)
+            
+            // form fields
+            VStack(spacing: 24) {
+                InputView(text: $email,
+                          title: "Email Address",
+                          placeholder: "name@example.com")
+                .textInputAutocapitalization(.none)
+                
+                InputView(text: $fullname,
+                          title: "Fullname",
+                          placeholder: "Enter your name")
+                
+                InputView(text: $password,
+                          title: "Password",
+                          placeholder: "Enter your password",
+                          isSecureField: true)
+                
+                InputView(text: $confirmPassword,
+                          title: "Confirm Password",
+                          placeholder: "Confirm Password",
+                          isSecureField: true)
+                
+                // sign in button
+                Button {
+                    print("Sign user up..")
+                } label: {
+                    HStack {
+                        Text("SIGN UP")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundStyle(.white)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                }
+                .background(Color(.systemPurple))
+                .cornerRadius(10)
+                .padding(.top, 24)
+                
+                Spacer()
+                Button {
+                    dismiss()
+                } label: {
+                    HStack(spacing: 10) {
+                        Text("Already have an account?")
+                        Text("Sign in")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size: 14))
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top, 12)
+        }
     }
 }
 
